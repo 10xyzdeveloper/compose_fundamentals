6b45bf4c4d53b5bfa8db0ce3d56ed635d1ca4904
Abhishek Chauhan <10xyzdeveloper@gmail.com>
2025-06-06 10:11:59 +0530

    Heart viewmodel added

--- app/src/main/java/com/house/myapplication/MyViewModel.kt
+++ app/src/main/java/com/house/myapplication/MyViewModel.kt
@@ -4,6 +4,7 @@ import androidx.lifecycle.ViewModel
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.update
 import javax.inject.Inject
 
 @HiltViewModel
@@ -13,5 +14,18 @@ class MyViewModel @Inject constructor(): ViewModel(){
         ListItem(id = index, title = "Item $index", isHearted = false)
     }))
     val uiState: StateFlow<UiState> = _uiState
+
+    fun toggleHeart(itemId:Int){
+        _uiState.update { current->
+            val updatedState = current.items.map { item->
+                if(item.id == itemId){
+                    item.copy(isHearted = !item.isHearted)
+                }else{
+                    item
+                }
+            }
+            current.copy(items = updatedState)
+        }
+    }
 
 }
